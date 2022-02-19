db5bff31acf342a5702bf3acc6cf3ef77d835524
Casey Vaughn <caseyvaughn@caseys-mbp-2.myfiosgateway.com>
2022-02-19 13:43:46 -0500

    created seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Zoo.destroy_all
+Panda.destroy_all
+
+zoo1 = Zoo.create! name: "San Diego Zoo", city: "San Diego"
+zoo2 = Zoo.create! name: "Bifengxia Panda Center", city: "Ya’an"
+zoo3 = Zoo.create! name: "Gonggashan", city: "Kangding"
+zoo4 = Zoo.create! name: "Edinburgh Zoo", city: "Edinburgh"
+zoo5 = Zoo.create! name: "General Assembly", city: "New York"
+
+panda1 = Panda.new name: "Bai Yun", age: 28
+panda2 = Panda.new name: "Gao Gao", age: 27
+panda3 = Panda.new name: "Tian Tian", age: 21
+panda4 = Panda.new name: "Basi", age: 37
+panda5 = Panda.new name: "Drake Talley", age: 102
+
+# associating the tables
+
+zoo1.panda << panda1
+zoo2.panda << panda2
+zoo3.panda << panda3
+zoo4.panda << panda4
+zoo5.panda << panda5
+
+puts "#{Zoo.all.size} zoo(s) were created!"
+puts "#{Panda.all.size} panda(s) were created!"
